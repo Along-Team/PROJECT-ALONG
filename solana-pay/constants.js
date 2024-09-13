982d973210e5e2f9985ffd8468b1c6dd57bdebb8
joel-danjuma <leojjad@gmail.com>
2024-09-13 12:35:15 +0100

    initial commit

---
+++ solana-pay/constants.js
@@ -0,0 +1,12 @@
+const { Keypair, PublicKey } = require("@solana/web3.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const MERCHANT_WALLET = new PublicKey(process.env.MERCHANT_WALLET_PUBLIC_KEY);
+
+// const CUSTOMER_WALLET = Keypair.fromSecretKey(Uint8Array.from([]))
+module.exports = {
+  MERCHANT_WALLET,
+  CUSTOMER_WALLET,
+};
