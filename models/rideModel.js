164cc3d026aaa09257532e7dc1a87fc26bcc1826
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2024-10-02 22:36:12 +0100

    ride update

--- models/rideModel.js
+++ models/rideModel.js
@@ -57,17 +57,17 @@ const rideSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    default: "Waiting",
-    enum: ["Waiting", "Moving", "Arrived"],
+    // default: "Waiting",
+    // enum: ["Waiting", "Moving", "Arrived"],
   },
-  moving: {
-    type: Boolean,
-    default: false,
-  },
-  arrived: {
-    type: Boolean,
-    default: false,
-  },
+  // moving: {
+  //   type: Boolean,
+  //   default: false,
+  // },
+  // arrived: {
+  //   type: Boolean,
+  //   default: false,
+  // },
 });
 
 rideSchema.index({ startLocation: "2dsphere" });
@@ -77,58 +77,6 @@ rideSchema.post("save", function (doc, next) {
   console.log(doc);
   next();
 });
-// driverSchema.pre("save", async function (next) {
-//   // Only run this function if password was actually modified
-//   if (!this.isModified("password")) return next();
-
-//   // Hash the password with cost of 12
-//   this.password = await bcrypt.hash(this.password, 12);
-
-//   // Delete password confirm field
-//   this.passwordConfirm = undefined;
-//   next();
-// });
-
-// driverSchema.pre(/^find/, function (next) {
-//   // this points to the current query
-//   this.find({ active: { $ne: false } });
-//   next();
-// });
-
-// driverSchema.methods.correctPassword = async function (
-//   candidatePassword,
-//   userPassword
-// ) {
-//   return await bcrypt.compare(candidatePassword, userPassword);
-// };
-
-// driverSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-//   if (this.passwordChangedAt) {
-//     const changedTimestamp = parseInt(
-//       this.passwordChangedAt.getTime() / 1000,
-//       10
-//     );
-//     // console.log(changedTimestamp, JWTTimestamp);
-//     return JWTTimestamp < changedTimestamp;
-//   }
-//   // False means not changed
-//   return false;
-// };
-
-// driverSchema.methods.createPasswordResetToken = function () {
-//   const resetToken = crypto.randomBytes(32).toString("hex");
-
-//   this.passwordResetToken = crypto
-//     .createHash("sha256")
-//     .update(resetToken)
-//     .digest("hex");
-
-//   console.log({ resetToken }, this.passwordResetToken);
-
-//   this.passwordResetExpires = Date.now() + 70 * 60 * 1000;
-
-//   return resetToken;
-// };
 
 const Ride = mongoose.model("Ride", rideSchema);
 
