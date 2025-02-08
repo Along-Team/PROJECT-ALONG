868ef18ac244b728c23e0388b0e91c403e89226b
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2025-02-08 14:02:40 +0100

    add wallet balance route

--- routes/moniepointRoute.js
+++ routes/moniepointRoute.js
@@ -4,5 +4,10 @@ const moniepointController = require("./../controllers/moniepointController");
 const router = express.Router();
 
 router.post("/createwallet", moniepointController.createWallet);
+router.get(
+  "/walletbalance/:accountNumber",
+  moniepointController.getWalletBalance
+);
+router.get("/allwallet", moniepointController.getAllWallet);
 
 module.exports = router;
